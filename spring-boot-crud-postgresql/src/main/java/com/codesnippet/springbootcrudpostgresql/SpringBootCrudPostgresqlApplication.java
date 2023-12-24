0cf9b6f2148f3cafdb0171cc2349039bc472be8f
Frutos Márquez Peláez <deigo81@gmail.com>
2023-12-24 12:50:27 +0100

    postgresql

---
+++ spring-boot-crud-postgresql/src/main/java/com/codesnippet/springbootcrudpostgresql/SpringBootCrudPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.codesnippet.springbootcrudpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudPostgresqlApplication.class, args);
+    }
+
+}
---
+++ spring-boot-crud-postgresql/src/test/java/com/codesnippet/springbootcrudpostgresql/SpringBootCrudPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesnippet.springbootcrudpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
